224eba598998c05e8f26361d553e9e4f63487f30
Antoine <antoine.carre@imt-atlantique.net>
2024-02-13 23:48:13 -0800

    added console

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,6 +13,7 @@
        console.log(Data);
    
    });
+   confsole.log(Data);
 </script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
