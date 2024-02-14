7ffec2461c3931cf85a90c6d1303a79dd0d99376
Antoine <antoine.carre@imt-atlantique.net>
2024-02-13 23:42:12 -0800

    code added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script>
+    let tempData = [];
+   
+   onMount(async () => {
+   
+       const res = await fetch('temperatures.csv'); 
+   
+       const csv = await res.text();
+   
+       tempData = d3.csvParse(csv, d3.autoType)
+   
+       console.log(tempData);
+   
+   });
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
