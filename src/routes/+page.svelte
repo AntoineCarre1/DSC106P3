0c7b92754665dda69e3f517403bd7e704831bf35
Antoine <antoine.carre@imt-atlantique.net>
2024-02-14 20:34:01 -0800

    up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
    
        const csv = await res.text();
    
-       parsedData = d3.csvParse(csv, d3.autoType)
+       parsedData = d3.csvParse(csv, d3.autoType);
    
        console.log("Initial Data:", parsedData);
 
