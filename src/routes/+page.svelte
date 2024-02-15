73193435bbaf6cea351f006d14aeac0a909bf100
Antoine <antoine.carre@imt-atlantique.net>
2024-02-15 12:56:51 -0800

    works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,3 @@
-<style>
-    /* CSS styling for the map */
-    path {
-        fill: none;
-        stroke: #000;
-        stroke-width: 1;
-    }
-</style>
-
 <script>
     import {onMount} from 'svelte';
     import * as d3 from 'd3';
@@ -21,29 +12,7 @@ onMount(async () => {
     Data = d3.csvParse(csv, d3.autoType);
     console.log("Data:", Data);
 });
-        // D3.js code to draw the outline of the world map
-    const svg = d3.select("svg");
-
-        // Define projection and path generator
-    const projection = d3.geoNaturalEarth1()
-                            .scale(150)
-                            .translate([480, 300]);
-    const pathGenerator = d3.geoPath().projection(projection);
-
-        // Load world GeoJSON data from Natural Earth
-    d3.json("https://raw.githubusercontent.com/nvkelso/natural-earth-vector/master/geojson/ne_110m_admin_0_countries.geojson").then(function(world) {
-            // Draw outline of the world map
-        svg.selectAll("path")
-            .data(world.features)
-            .enter().append("path")
-            .attr("d", pathGenerator);
-    }).catch(function(error) {
-        console.log(error);
-    });
 
 </script>
-
-<h1>World Map Outline with D3.js</h1>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<svg width="3000" height="2000"></svg>
