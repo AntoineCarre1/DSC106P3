d6479d21302d2b4c2e634a139d5a6e755e332d8d
Antoine <antoine.carre@imt-atlantique.net>
2024-02-20 15:59:49 -0800

    trry

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	// vite.config.js
+	build: {
+		// Add topojson-client as an external dependency
+		rollupOptions: {
+		  external: ['topojson-client'],
+		},
+	  },
 	plugins: [sveltekit()]
 });
