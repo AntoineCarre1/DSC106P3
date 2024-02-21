be6dd5ac4dd9a0e8b14e59843d94ec8bf98ea03f
Antoine <antoine.carre@imt-atlantique.net>
2024-02-20 16:13:03 -0800

    try

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,74 +1,53 @@
 <script>
     import { onMount } from 'svelte';
-    import * as d3 from 'd3';
-    import * as topojson from 'topojson-client';
+    import * as d3 from 'd3-geo';
+    import { geoAlbersUsa } from 'd3-geo-projection';
 
-    let usData;
     let svg;
-
+    let data = [];
     onMount(async () => {
-        const response = await fetch("https://d3js.org/us-10m.v1.json");
-        usData = await response.json();
+        const res = await fetch('API_SP.DYN.LE00.IN_DS2_en_csv_v2_46.csv');
+        const csv = await res.text();
+        Data = d3.csvParse(csv, d3.autoType);
+        console.log("Data:", Data);
+
+        const width = 975;
+        const height = 610;
+
+        const zoom = d3.zoom()
+            .scaleExtent([1, 8])
+            .on("zoom", zoomed);
 
         svg = d3.select("#my_dataviz")
-            .attr("viewBox", [0, 0, 975, 610])
-            .attr("width", 975)
-            .attr("height", 610)
+            .attr("viewBox", [0, 0, width, height])
+            .attr("width", width)
+            .attr("height", height)
             .attr("style", "max-width: 100%; height: auto;")
             .on("click", reset);
 
-        const path = d3.geoPath();
+        const projection = geoAlbersUsa().scale(1280).translate([width / 2, height / 2]);
+        const path = d3.geoPath(projection);
 
-        const g = svg.append("g");
-
-        const states = g.append("g")
-            .attr("fill", "#444")
-            .attr("cursor", "pointer")
-            .selectAll("path")
-            .data(topojson.feature(usData, usData.objects.states).features)
-            .join("path")
-            .on("click", clicked)
+        svg.append("path")
+            .datum(d3.geoGraticule())
+            .attr("class", "graticule")
             .attr("d", path);
-
-        states.append("title")
-            .text(d => d.properties.name);
-
-        g.append("path")
-            .attr("fill", "none")
-            .attr("stroke", "white")
-            .attr("stroke-linejoin", "round")
-            .attr("d", path(topojson.mesh(usData, usData.objects.states, (a, b) => a !== b)));
 
         svg.call(zoom);
 
         function reset() {
-            states.transition().style("fill", null);
             svg.transition().duration(750).call(
                 zoom.transform,
                 d3.zoomIdentity,
-                d3.zoomTransform(svg.node()).invert([975 / 2, 610 / 2])
-            );
-        }
-
-        function clicked(event, d) {
-            const [[x0, y0], [x1, y1]] = path.bounds(d);
-            event.stopPropagation();
-            states.transition().style("fill", null);
-            d3.select(this).transition().style("fill", "red");
-            svg.transition().duration(750).call(
-                zoom.transform,
-                d3.zoomIdentity
-                    .translate(975 / 2, 610 / 2)
-                    .scale(Math.min(8, 0.9 / Math.max((x1 - x0) / 975, (y1 - y0) / 610)))
-                    .translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
-                d3.pointer(event, svg.node())
+                d3.zoomTransform(svg.node()).invert([width / 2, height / 2])
             );
         }
 
         function zoomed(event) {
             const { transform } = event;
-            g.attr("transform", transform);
-            g.attr("stroke-width", 1 / transform.k);
+            svg.selectAll("path")
+                .attr("transform", transform)
+                .attr("stroke-width", 1 / transform.k);
         }
     });
 </script>
@@ -76,4 +55,3 @@
 <main>
     <svg id="my_dataviz" />
 </main>
-
