d2b98139db7082c704f73bea446c6dec599c418f
Antoine <antoine.carre@imt-atlantique.net>
2024-02-20 16:03:20 -0800

    Revert "trry"
    This reverts commit d6479d21302d2b4c2e634a139d5a6e755e332d8d.

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	// vite.config.js
-	build: {
-		// Add topojson-client as an external dependency
-		rollupOptions: {
-		  external: ['topojson-client'],
-		},
-	  },
 	plugins: [sveltekit()]
 });
